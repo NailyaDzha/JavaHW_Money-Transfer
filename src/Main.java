6fefbce0789ae9811e970c11c6baaabf1ecf982d
Nailya <botan4091@gmail.com>
2021-10-17 12:38:22 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentclientaccount = 2_000_000_000;
+        int transferamount = 500_000_000;
+        int total = currentclientaccount + transferamount;
+        System.out.println(total);
+    }
+}
